16fd0a10b83b472f63fc078eaa9c867bb8657f46
Nelson Izquierdo <izquierdonelson@gmail.com>
2025-06-22 01:04:21 -0400

    Refactor import paths to use absolute imports for UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Button } from "./components/ui/button";
-import { Label } from "./components/ui/label";
-import { Input } from "./components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 function App() {
   const [bill, setBill] = useState("0.00");
