e6174472287ce4b6845aa6ddfd081a0c3c5cb41f
Nelson Izquierdo <izquierdonelson@gmail.com>
2025-06-24 23:52:40 -0400

    Add PWA support with icons and manifest file
    - Added Android and Apple touch icons for the PWA.
    - Created a web manifest file to define the PWA's properties.
    - Updated vite.config.ts to include VitePWA plugin with configuration for the app's name, description, theme color, and icons.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,53 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "prompt", // Keeps the service worker updated
+      manifest: {
+        name: "Valentina Tip Calculator",
+        short_name: "Tip Calculator",
+        description: "A PWA for calculating tips and splitting bills",
+        theme_color: "#008cb1",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
